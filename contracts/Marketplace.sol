ff3be9ec9ef545c58ac131cc9c1bbbf220534bdb
web3joker <your-email@example.com>
2026-01-11 23:57:03 +0000

    feat: Update import paths for ReentrancyGuard in Marketplace, Staking, and Treasury contracts

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.24;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
 /**
  * @title Marketplace
